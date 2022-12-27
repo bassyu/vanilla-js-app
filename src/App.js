2dccb6c71bdb9a8d4a4c99c838aef866cb14bd9f
bassyu <yrkdgus@gmail.com>
2022-12-27 14:25:16 +0900

    feat(App): Node 클릭 이벤트 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Breadcrumb from './components/Breadcrumb.js';
+import Modal from './components/Modal.js';
 import Nodes from './components/Nodes.js';
-import { request } from './lib/api.js';
+import { getNodes } from './lib/api.js';
 
 class App {
   constructor({ parent }) {
@@ -8,28 +9,50 @@ class App {
       isRoot: true,
       path: [],
       nodes: [],
+      filePath: null,
     };
 
     this.breadcrumb = new Breadcrumb({ parent });
-    this.nodes = new Nodes({ parent });
+    this.nodes = new Nodes({ parent, onClickNode: this.onClickNode.bind(this) });
+    this.modal = new Modal({ parent });
 
-    this.callAPI('');
+    this.setNodes('');
   }
 
   setState(newState) {
     this.state = newState;
-    const { isRoot, path, nodes } = newState;
+    const { isRoot, path, nodes, filePath } = newState;
     this.breadcrumb.setState({ path });
     this.nodes.setState({ isRoot, nodes });
+    this.modal.setState({ filePath });
+    console.log(this.state);
   }
 
-  async callAPI(id) {
-    const nodes = await request(id);
+  async setNodes(id) {
     this.setState({
       ...this.state,
-      isRoot: true,
+      filePath: 'loading',
+    });
+    const nodes = await getNodes(id);
+    this.setState({
+      ...this.state,
+      filePath: null,
+      isRoot: id === '',
       nodes,
     });
+  }
+
+  async onClickNode(e) {
+    const id = e.currentTarget.id;
+    const clickedNode = this.state.nodes.find((node) => node.id === id);
+    if (clickedNode.type === 'DIRECTORY') {
+      this.setNodes(clickedNode.id);
+    } else if (clickedNode.type === 'FILE') {
+      this.setState({
+        ...this.state,
+        filePath: clickedNode.filePath,
+      });
+    }
   }
 }
 
--- src/components/Nodes.js
+++ src/components/Nodes.js
@@ -1,5 +1,5 @@
 class Nodes {
-  constructor({ parent }) {
+  constructor({ parent, onClickNode }) {
     this.state = {
       isRoot: true,
       nodes: [],
@@ -7,29 +7,36 @@ class Nodes {
 
     this.component = document.createElement('div');
     this.component.className = 'Nodes';
-
     parent.appendChild(this.component);
+
+    this.onClickNode = onClickNode;
   }
 
   setState(newState) {
     this.state = newState;
     this.render();
+    console.log(this.state);
   }
 
   render() {
+    console.log('render', this.state);
     this.component.innerHTML = `
       ${this.state.isRoot ? '' : '<div class="Node"><img src="./assets/prev.png"></div>'}
       ${this.state.nodes
         .map(
           (node) => `
             <div class="Node" id="${node.id}">
-              <img src="${node.type === 'FILE' ? node.filePath : './assets/directory.png'}">
+              <img src="${node.type === 'FILE' ? './assets/file.png' : './assets/directory.png'}">
               <div>${node.name}</div>
             </div>
           `
         )
         .join('')}
     `;
+
+    this.component.querySelectorAll('.Node').forEach((nodeElement) => {
+      nodeElement.addEventListener('click', this.onClickNode);
+    });
   }
 }
 
