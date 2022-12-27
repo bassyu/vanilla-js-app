e86b9f9399ecaa00b7560471374093ed4aa7724d
bassyu <yrkdgus@gmail.com>
2022-12-27 14:24:29 +0900

    style(api): 공백 제거

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,6 @@ const BASE_URL = 'https://l9817xtkq3.execute-api.ap-northeast-2.amazonaws.com/de
 
 export const getNodes = async (id) => {
   const fullURL = `${BASE_URL}/${id}`;
-
   try {
     const response = await fetch(fullURL);
     if (response.ok) {
