0770af885a919cbcfacb78737d5eb3b290783c38
bassyu <yrkdgus@gmail.com>
2022-12-27 13:31:30 +0900

    refactor(api): getNodes 수정
    - 네이밍 변경
    - 조건문 구조 변경

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,16 +1,16 @@
 const BASE_URL = 'https://l9817xtkq3.execute-api.ap-northeast-2.amazonaws.com/dev';
 
-export const request = async (id) => {
+export const getNodes = async (id) => {
   const fullURL = `${BASE_URL}/${id}`;
 
   try {
     const response = await fetch(fullURL);
-    if (!response.ok) {
+    if (response.ok) {
+      const nodes = await response.json();
+      return nodes;
+    } else {
       throw new Error();
     }
-
-    const nodes = await response.json();
-    return nodes;
   } catch (e) {
     console.log(e);
   }
