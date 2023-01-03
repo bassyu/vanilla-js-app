66f73648de49af46bfb49518a02bd2e33fc48655
bassyu <yrkdgus@gmail.com>
2023-01-03 12:57:02 +0900

    feat(Breadcrumb): 경로 클릭 이벤트 적용

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -1,13 +1,14 @@
 class Breadcrumb {
-  constructor({ parent }) {
+  constructor({ parent, onClickPathName }) {
     this.state = {
       path: [],
     };
 
     this.component = document.createElement('nav');
     this.component.className = 'Breadcrumb';
-
     parent.appendChild(this.component);
+
+    this.onClickPathName = onClickPathName;
   }
 
   setState(newState) {
@@ -17,9 +18,15 @@ class Breadcrumb {
 
   render() {
     this.component.innerHTML = `
-      <div>root</div>
-      ${this.state.path.map((node, index) => `<div id="${index}">${node.name}</div>`).join('')}
+      <div class="pathName" id="root">root</div>
+      ${this.state.path
+        .map((node, index) => `<div class="pathName" id="${index}">${node.name}</div>`)
+        .join('')}
     `;
+
+    this.component.querySelectorAll('.pathName').forEach((pathName) => {
+      pathName.addEventListener('click', this.onClickPathName);
+    });
   }
 }
 
